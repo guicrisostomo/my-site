b8bf4263cd486fc228f509acd7672d0833f251fa
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-08-03 22:42:56 -0300

    Reverse changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home';
 import Skill from './Pages/Skills';
@@ -8,7 +9,14 @@ import Contact from './Pages/Contact';
 import Error from './Pages/Error';
 
 ReactDOM.render(
-  <Home />,
+  <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/skill" component={Skill} />
+            <Route path="/contact" component={Contact} />
+            <Route path="*" component={Error} />
+        </Switch>
+    </ BrowserRouter>,
   document.getElementById('root')
 );
 
