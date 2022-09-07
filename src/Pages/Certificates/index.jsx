f7a0bb67802cc1c0e7f1531cbf33395724124f16
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-07 20:15:21 -0300

    fix: navbar dont recognize page certificates

--- src/Pages/Certificates/index.jsx
+++ src/Pages/Certificates/index.jsx
@@ -18,48 +18,53 @@ import pdfIntroductionNetwork from '../../assets/certificates/CCNAv7 Introduçã
 import pdfIntroductionCybersecurity from '../../assets/certificates/Introduction to Cybersecurity.pdf';
 import pdfLinux from '../../assets/certificates/NDG Linux Unhatched Português.pdf';
 import { useTranslation } from 'react-i18next';
+import Header from '../../components/Header';
 
 export function Certificates() {
     const { t } = useTranslation()
 
     return(
-        <SectionCertifications>
-            <TextCertificates>
-                {t('certificates.title')}
-            </TextCertificates>
+        <>
+            <Header />
 
-            <Items>
-                <ItemCertificates href={pdfMRV} target='_blank' urlImage={imgDIO}>
-                    <TextInfoCertificates>MRV Fullstack Developer</TextInfoCertificates>
-                </ItemCertificates>
+            <SectionCertifications>
+                <TextCertificates>
+                    {t('certificates.title')}
+                </TextCertificates>
 
-                <ItemCertificates href={pdfMobileAppDevelopment} target='_blank' urlImage={imgIFSP}>
-                    <TextInfoCertificates>Programação de Computadores e Dispositivos Móveis
-                    </TextInfoCertificates>
-                </ItemCertificates>
+                <Items>
+                    <ItemCertificates href={pdfMRV} target='_blank' urlImage={imgDIO}>
+                        <TextInfoCertificates>MRV Fullstack Developer</TextInfoCertificates>
+                    </ItemCertificates>
 
-                <ItemCertificates href={pdfDevelopmentWEB} target='_blank'  urlImage={imgIFSP}>
-                    <TextInfoCertificates>
-                    Introdução à Programação WEB
-                    </TextInfoCertificates>
-                </ItemCertificates>
+                    <ItemCertificates href={pdfMobileAppDevelopment} target='_blank' urlImage={imgIFSP}>
+                        <TextInfoCertificates>Programação de Computadores e Dispositivos Móveis
+                        </TextInfoCertificates>
+                    </ItemCertificates>
 
-                <ItemCertificates href={pdfIntroductionNetwork} target='_blank' urlImage={imgCisco}>
-                    <TextInfoCertificates>Introdução às redes</TextInfoCertificates>
-                </ItemCertificates>
+                    <ItemCertificates href={pdfDevelopmentWEB} target='_blank'  urlImage={imgIFSP}>
+                        <TextInfoCertificates>
+                        Introdução à Programação WEB
+                        </TextInfoCertificates>
+                    </ItemCertificates>
 
-                <ItemCertificates href={pdfIntroductionCybersecurity} target='_blank'  urlImage={imgCisco}>
-                    <TextInfoCertificates>
-                    Introduction to Cybersecurity
-                    </TextInfoCertificates>
-                </ItemCertificates>
+                    <ItemCertificates href={pdfIntroductionNetwork} target='_blank' urlImage={imgCisco}>
+                        <TextInfoCertificates>Introdução às redes</TextInfoCertificates>
+                    </ItemCertificates>
 
-                <ItemCertificates href={pdfLinux} target='_blank'  urlImage={imgCisco}>
-                    <TextInfoCertificates>NDG Linux Unhatched
-                    </TextInfoCertificates>
-                </ItemCertificates>
+                    <ItemCertificates href={pdfIntroductionCybersecurity} target='_blank'  urlImage={imgCisco}>
+                        <TextInfoCertificates>
+                        Introduction to Cybersecurity
+                        </TextInfoCertificates>
+                    </ItemCertificates>
 
-            </Items>
-        </SectionCertifications>
+                    <ItemCertificates href={pdfLinux} target='_blank'  urlImage={imgCisco}>
+                        <TextInfoCertificates>NDG Linux Unhatched
+                        </TextInfoCertificates>
+                    </ItemCertificates>
+
+                </Items>
+            </SectionCertifications>
+        </>
     )
 }
