3637c086c4cab2255d7b324e7801e9774818a997
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-12 20:50:48 -0300

    feat: add item projects

--- src/Pages/Projects/index.jsx
+++ src/Pages/Projects/index.jsx
@@ -3,7 +3,18 @@ import Header from '../../components/Header';
 import { 
     SectionProjects,
     TextProjects,
+    Items,
+    ItemProject,
+    ItemProjectImg,
+    ItemProjectText,
+    ItemLanguagesProject,
+    TextTitleProject,
+    TextInfoProject,
+    ImgProject
 } from "../Projects/style";
+
+import imgIfood from '../../assets/img/imgIfood.svg'
+import { SiHtml5, SiCss3, SiJavascript, SiReact } from 'react-icons/si';
 
 export function Projects() {
     return(
@@ -14,6 +25,33 @@ export function Projects() {
                 <TextProjects>
                     Projects
                 </TextProjects>
+
+                <Items>
+                    <ItemProject href='https://github.com/guicrisostomo/Projetos-DIO/tree/master/React/Desafio%20-%20criando%20interface%20do%20iFood' target='_blank'>
+                        <ItemProjectImg>
+                            <ImgProject src={imgIfood} />
+                        </ItemProjectImg>
+                        
+                        <ItemProjectText>
+                            <TextTitleProject>Clone ifood</TextTitleProject>
+
+                            <TextInfoProject>Projeto criado no bootcamp MRV FullStack Developer da DIO</TextInfoProject>
+
+                            <ItemLanguagesProject>
+
+                                <SiHtml5 style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
+
+                                <SiCss3 style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
+
+                                <SiJavascript style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
+
+                                <SiReact style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
+
+                            </ItemLanguagesProject>
+                        </ItemProjectText>
+                        
+                    </ItemProject>
+                </Items>
             </SectionProjects>
         </>
     )
--- src/Pages/Projects/style.js
+++ src/Pages/Projects/style.js
@@ -26,3 +26,84 @@ export const TextProjects = styled.h1`
         font-size: 20px;
     }
 `;
+
+export const Items = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
+
+export const ItemProject = styled.a`
+    width: 100%;
+    color: white;
+    display: flex;
+    flex-wrap: wrap;
+    background-color: #000000a1;
+    align-items: center;
+    border-radius: 5px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: white;
+    padding: 42px 42px 42px 42px;
+    box-shadow: 11px 11px 50px white;
+    margin-top: 20px;
+    cursor: pointer;
+    text-decoration: none;
+    transform: scale(80%, 80%);
+    transition: 1s all;
+
+    :hover {
+        background-color: white;
+        color: black;
+        transform: scale(90%, 90%);
+    }
+`;
+
+export const TextTitleProject = styled.h2`
+    padding-top: 10px;
+    margin-right: 10px;
+
+    @media (max-width: 640px) {
+        align-self: center;
+    }
+
+    @media (max-width: 400px) {
+        font-size: 16px;
+    }
+`;
+
+export const TextInfoProject = styled.p`
+    
+`;
+
+export const ImgProject = styled.img`
+    height: 100px;
+    width: 100px;
+`;
+
+export const ItemProjectImg = styled.div`
+    display: flex;
+    padding: 10px;
+
+    @media (max-width: 640px) {
+        width: 100%;
+        place-content: center;
+    }
+`;
+
+export const ItemProjectText = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+
+    @media (max-width: 640px) {
+        width: 100%;
+    }
+`;
+
+export const ItemLanguagesProject = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+`;
