3b9d9fe016e8fb78a841501793949d8326e90e5c
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-12 01:43:50 -0300

    fix: add verification error

--- src/components/DataProjects/index.jsx
+++ src/components/DataProjects/index.jsx
@@ -34,52 +34,59 @@ export function DataProjects() {
     
     return(
         projects.map( (value) =>
-            <Items key={value.nomeEN}>
-                                
-                <ItemProject href={value.link} target='_blank'>
-                    <ItemProjectImg>
-                        <ImgProject src={value.linkImage} />
-                    </ItemProjectImg>
-                    
-                    <ItemProjectText>
-                        <TextTitleProject>
-                            {i18n.language === "pt-BR" && (
-                                value.nomePT
-                            )}
+        runCallback(() => {
+            try {
+                return (
+                    <Items key={value.nomeEN}>
+                                    
+                        <ItemProject href={value.link} target='_blank'>
+                            <ItemProjectImg>
+                                <ImgProject src={value.linkImage} />
+                            </ItemProjectImg>
+                            
+                            <ItemProjectText>
+                                <TextTitleProject>
+                                    {i18n.language === "pt-BR" && (
+                                        value.nomePT
+                                    )}
 
-                            {i18n.language === "en-US" && (
-                                value.nomeEN
-                            )}
-                        </TextTitleProject>
+                                    {i18n.language === "en-US" && (
+                                        value.nomeEN
+                                    )}
+                                </TextTitleProject>
 
-                        <TextInfoProject>
-                            {i18n.language === "pt-BR" && (
-                                value.descricaoPT
-                            )}
+                                <TextInfoProject>
+                                    {i18n.language === "pt-BR" && (
+                                        value.descricaoPT
+                                    )}
 
-                            {i18n.language === "en-US" && (
-                                value.descricaoEN
-                            )}
-                        </TextInfoProject>
+                                    {i18n.language === "en-US" && (
+                                        value.descricaoEN
+                                    )}
+                                </TextInfoProject>
 
-                        <ItemLanguagesProject>
-                            {
-                                runCallback(() => {
-                                    const row = [];
+                                <ItemLanguagesProject>
+                                    {
+                                        runCallback(() => {
+                                            const row = [];
 
-                                    for (var i = 0; i < value.skills.length; i++) {
-                                        row.push(<ItemSkillImg src={value.skills[i]} ImgDark={value.skillDark} key={'skill' + i}/>);
+                                            for (var i = 0; i < value.skills.length; i++) {
+                                                row.push(<ItemSkillImg src={value.skills[i]} ImgDark={value.skillDark} key={'skill' + i}/>);
+                                            }
+
+                                            return row;
+                                        })
                                     }
 
-                                    return row;
-                                })
-                            }
-
-                        </ItemLanguagesProject>
-                    </ItemProjectText>
-                    
-                </ItemProject>
-            </Items>
-        )
+                                </ItemLanguagesProject>
+                            </ItemProjectText>
+                            
+                        </ItemProject>
+                    </Items>
+                )
+            } catch (error) {
+                
+            }
+        }))
     );
 }
