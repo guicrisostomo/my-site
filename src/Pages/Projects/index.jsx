4331d227f906c1c091a3a4715a80d06336576098
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-11 22:22:44 -0300

    refactor: remove code commented

--- src/Pages/Projects/index.jsx
+++ src/Pages/Projects/index.jsx
@@ -20,64 +20,6 @@ export function Projects() {
                     {t('projects.title.main')}
                 </TextProjects>
 
-                {/*
-                <Items>
-                    <ItemProject href='https://github.com/guicrisostomo/Projetos-DIO/tree/master/React/Desafio%20-%20criando%20interface%20do%20iFood' target='_blank'>
-                        <ItemProjectImg>
-                            <ImgProject src={imgIfood} />
-                        </ItemProjectImg>
-                        
-                        <ItemProjectText>
-                            <TextTitleProject>
-                                {t('projects.title.item1')}
-                            </TextTitleProject>
-
-                            <TextInfoProject>
-                                {t('projects.info.item1')}
-                            </TextInfoProject>
-
-                            <ItemLanguagesProject>
-
-                                <SiHtml5 style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
-
-                                <SiCss3 style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
-
-                                <SiJavascript style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
-
-                                <SiReact style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
-
-                            </ItemLanguagesProject>
-                        </ItemProjectText>
-                        
-                    </ItemProject>
-                </Items>
-
-                <Items>
-                    <ItemProject href='https://github.com/guicrisostomo/tcc' target='_blank'>
-                        <ItemProjectImg>
-                            <IoPizzaOutline style={{width: 100, height: 100}} />
-                        </ItemProjectImg>
-                        
-                        <ItemProjectText>
-                            <TextTitleProject>
-                                {t('projects.title.item2')}
-                            </TextTitleProject>
-
-                            <TextInfoProject>
-                                {t('projects.info.item2')}
-                            </TextInfoProject>
-
-                            <ItemLanguagesProject>
-
-                                <SiFlutter style={{display: 'flex', width: 50, height: 50, marginRight: '10px', borderRadius: '50%', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white', padding: '5px'}}/>
-
-                            </ItemLanguagesProject>
-                        </ItemProjectText>
-                        
-                    </ItemProject>
-                </Items>
-                */}
-
                 <DataProjects />
 
             </SectionProjects>
