ec7b223858198855721ebc0a0099a585dba1b65e
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-30 13:49:42 -0300

    refactor: remove console.log

--- src/assets/database/config/index.js
+++ src/assets/database/config/index.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-console.log(process.env.REACT_APP_FIREBASE_MEASUREMENTID)
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
