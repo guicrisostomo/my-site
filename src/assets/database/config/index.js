c7f68dfdf8b88f2b2d27c8d6a0c3db8b2c7d6c87
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-11-02 12:49:36 -0300

    refactor: change name environment variables

--- src/assets/database/config/index.js
+++ src/assets/database/config/index.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+    apiKey: process.env.REACT_APP_VERCEL_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_VERCEL_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_VERCEL_FIREBASE_DATABASEURL,
+    projectId: process.env.REACT_APP_VERCEL_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_VERCEL_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_VERCEL_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_VERCEL_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_VERCEL_FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(config);
