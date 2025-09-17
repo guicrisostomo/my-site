7e1cf65185f9043188ea82edeac3fe1ea868da7e
Guilherme Crisostomo da Silva <guilhermecrisostomo75@gmail.com>
2025-09-17 20:12:30 -0300

    arruma texto

--- src/assets/i18n/locales/en-us.js
+++ src/assets/i18n/locales/en-us.js
@@ -19,7 +19,7 @@ export default {
     home: {
       about: {
         title: "About me",
-        text: "Currently, I have a degree in Systems Analysis and Development (ADS) from FATEC and a Front-end developer from PA Informática. As for my professional life, I work with React, along with tools such as TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. I have already worked on creating websites according to figma; viewing and sending images and other documents to the API; creating graphics through the data obtained by the back-end; creating sessions using cookies with JWT standard; sending authorization tokens to the API. I have always loved challenges and solving problems (things that made me like the IT area a lot)! As for my personal life, I love to code in my spare time, play on the console/PC, watch series and chat with friends. I am good at working in a team and I am proactive!",
+        text: "Currently, I have a degree in Systems Analysis and Development (ADS) from FATEC and a manager from La Pasta. As for my professional life, I work with React, along with tools such as TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. I have already worked on creating websites according to figma; viewing and sending images and other documents to the API; creating graphics through the data obtained by the back-end; creating sessions using cookies with JWT standard; sending authorization tokens to the API. I have always loved challenges and solving problems (things that made me like the IT area a lot)! As for my personal life, I love to code in my spare time, play on the console/PC, watch series and chat with friends. I am good at working in a team and I am proactive!",
       },
 
       event: {
@@ -58,7 +58,7 @@ export default {
         date: "September 2023 - April 2025",
         position: "Front-end Developer",
         description:
-          "I work with React, along with tools such as TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. I have already worked on creating websites according to figma; viewing and sending images and other documents to the API; creating graphics through the data obtained by the back-end; creating sessions using cookies with JWT standard; sending authorization tokens to the API.",
+          "I worked with React, along with tools such as TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. I have already worked on creating websites according to figma; viewing and sending images and other documents to the API; creating graphics through the data obtained by the back-end; creating sessions using cookies with JWT standard; sending authorization tokens to the API.",
         skills: [
           "React",
           "Vite",
--- src/assets/i18n/locales/pt-br.js
+++ src/assets/i18n/locales/pt-br.js
@@ -21,7 +21,7 @@ export default {
     home: {
       about: {
         title: "Sobre mim",
-        text: "Atualmente, sou formado em Analise e Desenvolvimento de Sistemas (ADS) pela FATEC e desenvolvedor Front-end pela PA Informática. Quanto a minha vida profissional, atuo com React, juntamente com ferramentas como TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. Já atuei na criação de sites conforme figma; visualização e envio imagens e outros documentos para API; elaboração gráficos por meio dos dados obtidos pelo back-end; criação sessões utilizando cookies com padrão JWT; envio tokens de autorização para a API. Sempre adorei desafios e solucionar problemas (coisas pelas quais me fizeram gostar e muito da área de TI)! Quanto a minha vida particular, adoro codar na horas vagas, jogar no console/PC, assistir séries e conversar com amigos! Sou bom em trabalhar em equipe e sou proativo!",
+        text: "Atualmente, sou formado em Analise e Desenvolvimento de Sistemas (ADS) pela FATEC e gerente pela La Pasta. Quanto a minha vida profissional, atuo com React, juntamente com ferramentas como TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. Já atuei na criação de sites conforme figma; visualização e envio imagens e outros documentos para API; elaboração gráficos por meio dos dados obtidos pelo back-end; criação sessões utilizando cookies com padrão JWT; envio tokens de autorização para a API. Sempre adorei desafios e solucionar problemas (coisas pelas quais me fizeram gostar e muito da área de TI)! Quanto a minha vida particular, adoro codar na horas vagas, jogar no console/PC, assistir séries e conversar com amigos! Sou bom em trabalhar em equipe e sou proativo!",
       },
 
       event: {
@@ -63,7 +63,7 @@ export default {
         date: "Setembro de 2023 - Abril de 2025",
         position: "Desenvolvedor Front-end",
         description:
-          "Trabalho com React, juntamente com ferramentas como TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. Já atuei na criação de sites conforme figma; visualização e envio imagens e outros documentos para API; elaboração gráficos por meio dos dados obtidos pelo back-end; criação sessões utilizando cookies com padrão JWT; envio tokens de autorização para a API.",
+          "Trabalhei com React, juntamente com ferramentas como TypeScript, Vite, Tailwind CSS, Shadcn, Redux, React Forms, Zod. Já atuei na criação de sites conforme figma; visualização e envio imagens e outros documentos para API; elaboração gráficos por meio dos dados obtidos pelo back-end; criação sessões utilizando cookies com padrão JWT; envio tokens de autorização para a API.",
         company: "PA Informática",
         skills: [
           "React",
